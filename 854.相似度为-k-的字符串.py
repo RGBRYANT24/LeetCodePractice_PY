d5451959842e480d0726c00be93d7275d9af7c2e
ADRIN <1021741772@qq.com>
2022-09-21 19:00:17 +0800

    针对每一层的bfs 可以把每一层单独存放一个队列

--- 854.相似度为-k-的字符串.py
+++ 854.相似度为-k-的字符串.py
@@ -54,6 +54,7 @@ class Solution:
     def kSimilarity(self, s1: str, s2: str) -> int:
         step, n = 0, len(s1)
         q, vis = [(s1, 0)], {s1}
+        step = 0
         while True:
             tmp = q
             q = []
@@ -62,17 +63,18 @@ class Solution:
                     return step
                 while i < n and s[i] == s2[i]:
                     i += 1
-                for j in range(i + 1, n):
-                    if s[j] == s2[i] != s2[j]:  # 剪枝，只在 s[j] != s2[j] 时去交换
-                        t = list(s)
-                        t[i], t[j] = t[j], t[i]
-                        t = ''.join(t)
-                        if t not in vis:
-                            vis.add(t)
-                            q.append((t, i + 1))
+                for j in range(i+1, n):
+                    if s[j] == s2[i] != s2[j]: # 只有在匹配上正确的字符串 并且交换有意义的时候交换 （剪枝了）
+                        temp_s = list(s)
+                        temp_s[i], temp_s[j] = temp_s[j], temp_s[i]
+                        temp_s = ''.join(temp_s)
+                        if temp_s not in vis:
+                            q.append((temp_s, i+1))
+                            vis.add(temp_s)
             step += 1
-
-            
+'''
+针对每一层的bfs 可以把每一层单独存放一个队列
+'''           
         
 
 '''
