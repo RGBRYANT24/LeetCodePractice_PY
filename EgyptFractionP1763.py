6810931f728f60744841e3655aca2d0405f9921d
RGBRYANT24 <1021741772@qq.com>
2022-10-09 22:19:49 +0800

    huajian

--- EgyptFractionP1763.py
+++ EgyptFractionP1763.py
@@ -8,3 +8,10 @@ def gcd(a, b):
 def yf(a: int, b: int):
     tmp = gcd(a, b)
     return a/tmp, b/tmp
+
+def sum_num(num1 : tuple, num2 : tuple):
+    result = (0 ,0)
+    result[0] = num1[0] + num2[0]
+    result[1] = num1[1] + num2[1]
+    tmp = gcd(result[0], result[1])
+    
