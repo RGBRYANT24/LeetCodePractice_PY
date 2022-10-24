0578d562769d11a64690c12a40533960418beffb
RGBRYANT24 <1021741772@qq.com>
2022-10-24 18:25:13 +0800

    优化建图才是重点 终于AC了  写一年多的题

--- 127.单词接龙.py
+++ 127.单词接龙.py
@@ -5,11 +5,42 @@
 #
 
 # @lc code=start
+import collections
+from gc import collect
 from queue import Empty
 
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        nodeNum = 0
+        wordId = dict()
+        edge = collections.defaultdict(list)
+        def addWord(word: str):
+            if word not in wordId:
+                nonlocal nodeNum
+                wordId[word] = nodeNum
+                nodeNum += 1
+        
+        def addEdge(word: str):
+            addWord(word)
+            id1 = wordId[word]
+            chars = list(word)
+            for i in range(len(chars)):
+                tmp = chars[i]
+                chars[i] = "*"
+                newWord = "".join(chars)
+                addWord(newWord)
+                id2 = wordId[newWord]
+                edge[id1].append(id2)
+                edge[id2].append(id1)
+                chars[i] = tmp
+
+        for word in wordList:
+            addEdge(word)
+        addEdge(beginWord)
+        if endWord not in wordId:
+            return 0
+
         if beginWord == endWord:
             return 0
         if endWord not in wordList:
@@ -29,40 +60,59 @@ class Solution:
         if check(beginWord, endWord):
             return 2
 
-        
+        word_edge = collections.defaultdict(list)
 
-        q, q_reverse = [(beginWord, 1)], [(endWord, 1)]
+        q, q_reverse = [(wordId[beginWord], 0)], [(wordId[endWord], 0)]
+        # return (wordId[beginWord], wordId[endWord])
         d = dict() # 存储是否访问过的dict key : step
+        d_reverse = dict()
         count = 0
         ans = 0
-        while  len(q) > 0 and len(q_reverse) > 0 and count < 1e7:
+        word_order = []
+        while  len(q) > 0 or len(q_reverse) > 0 and count < 1e7:
             count += 1
             # if len(q) < len(q_reverse): # 选择元素少的队列开始bfs
-            if True:
-                node, step = q[0]
-                q.pop(0)
-                if node in d:    # 目标已经访问过了
-                    ans = d[node] + step
-                    break
-                for word in wordList:  # 正常bfs
-                    if (word not in d) and (check(node, word)):
-                        q.append((word, step + 1))
-                        d[node] = step + 1
-
-            if True:
-            # else:
-                node, step = q_reverse[0]
-                q_reverse.pop(0)
-                if node in d:    # 目标已经访问过了
-                    ans = d[node] + step
-                    break
-                for word in wordList:  # 正常bfs
-                    if (word not in d) and (check(node, word)):
-                        q_reverse.append((word, step + 1))
-                        d[node] = step + 1
-
-
-        return ans - 1
+            if len(q) > 0:
+                q_size = len(q)
+                for _ in range(q_size):
+                    node, step = q[0]
+                    q.pop(0)
+                    if node in d_reverse:    # 目标已经访问过了
+                        ans = (d_reverse[node] + step) // 2 + 1
+                        # return (d_reverse[node], step, node)
+                        # ans = d_reverse[node] + step -1
+                        # return "666"
+                        return ans
+                    d[node] = step
+                    next_words = word_edge[node]
+                    for word in edge[node]:  # 正常bfs
+                        if word not in d:
+                            # d[word] = step + 1
+                            q.append((word, step + 1))
+                
+            
+            if len(q_reverse) > 0:
+            # else: 
+                q_reverse_size = len(q_reverse)
+                for _ in range(q_reverse_size):
+                    node, step = q_reverse[0]                
+                    q_reverse.pop(0)
+                    word_order.append(node)
+                    if node in d:    # 目标已经访问过了
+                        ans = (d[node] + step) // 2 + 1
+                        # ans = d[node] + step -1
+                        # return (d[node], step, node)
+                        return ans
+                    d_reverse[node] = step
+                    next_words = word_edge[node]
+                    for word in edge[node]:  # 正常bfs
+                        if word not in d_reverse:
+                            # word_edge.append((node, word, step))
+                            '''if word == 'yecij' and step == 2:
+                                return (node ,check(word, node), word, step, word in d)'''
+                            # d_reverse[node] = step + 1
+                            q_reverse.append((word, step + 1))       
+        return ans
 
 # @lc code=end
 
