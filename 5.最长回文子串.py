c4f08dad319fba566b135558aa7dc0639fb87b55
RGBRYANT24 <1021741772@qq.com>
2022-11-03 12:16:56 +0800

    从小字符串向大字符串转移 每次枚举的应该是区间长度

--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -11,37 +11,37 @@ class Solution:
         if n < 2:
             return s
         
-        max_len = 1
-        begin = 0
-        # dp[i][j] 表示 s[i..j] 是否是回文串
-        dp = [[False] * n for _ in range(n)]
+        dp = [[False] * n for _  in range(n)]
         for i in range(n):
             dp[i][i] = True
         
-        # 递推开始
-        # 先枚举子串长度
-        for L in range(2, n + 1):
-            # 枚举左边界，左边界的上限设置可以宽松一些
+        maxi, maxj = 0, 0
+        
+        # traverse_list = []
+        for L in range(2, n+1): # 枚举的是区间长度
             for i in range(n):
-                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
-                j = L + i - 1
-                # 如果右边界越界，就可以退出当前循环
+                j = L + i - 1 # j是右侧边界 因为L = j - i + 1推出
                 if j >= n:
-                    break
-                    
+                    continue
+                
                 if s[i] != s[j]:
-                    dp[i][j] = False 
+                    dp[i][j] = False
+                
                 else:
                     if j - i < 3:
                         dp[i][j] = True
                     else:
                         dp[i][j] = dp[i + 1][j - 1]
                 
-                # 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
-                if dp[i][j] and j - i + 1 > max_len:
-                    max_len = j - i + 1
-                    begin = i
-        return s[begin:begin + max_len]
+                if dp[i][j]:
+                    if L > maxj - maxi + 1:
+                        maxi, maxj = i, j
+                # traverse_list.append((i, j, dp[i][j]))
+        # return traverse_list
+        # return maxi, maxj
+        return s[maxi : maxj + 1]
+                    
+
 
 
 # @lc code=end
