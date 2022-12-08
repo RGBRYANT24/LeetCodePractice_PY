5289ac8c8aa2d2ce5780df65ba0019968bad8b09
ADRIN <1021741772@qq.com>
2022-12-08 19:15:56 +0800

    左闭右开 返回下界大于等于

--- 704.二分查找.py
+++ 704.二分查找.py
@@ -7,15 +7,15 @@
 # @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        while (l <= r):
-            c = l + (r - l) // 2
-            if nums[c] == target:
-                return c
-            if nums[c] < target:
-                l = c + 1
+        l, r = 0, len(nums)
+        while(l < r): # 左闭右开
+            mid = int(l + (r - l) // 2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                l = mid + 1
             else:
-                r = c - 1
+                r = mid
         return -1
 # @lc code=end
 
